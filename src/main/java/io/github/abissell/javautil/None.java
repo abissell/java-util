c06dc8268744b01d98ef6784ca14a4713209089f
Andrew Bissell <abissell@gmail.com>
2023-06-01 08:44:46 +0000

    change groupId to io.github.abissell

--- src/main/java/io/github/abissell/javautil/None.java
+++ src/main/java/io/github/abissell/javautil/None.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.abissell.javautil;
+package io.github.abissell.javautil;
 
 public /* value */ record None<T>() implements Opt<T> {
     private static final None<?> NONE = new None<>();
--- src/main/java/io/github/abissell/javautil/Some.java
+++ src/main/java/io/github/abissell/javautil/Some.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.abissell.javautil;
+package io.github.abissell.javautil;
 
 import java.util.Objects;
 
