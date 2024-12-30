53b9a09c6f438b128bf52ccf5958e2b5e4acb4dc
Andrew Bissell <abissell@gmail.com>
2024-12-30 02:57:06 +0000

    modularize the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.abissell.javautil {
+    exports com.abissell.javautil.concurrent;
+    exports com.abissell.javautil.io;
+    exports com.abissell.javautil.math;
+    exports com.abissell.javautil.rusty;
+
+    requires chronicle.core;
+}
