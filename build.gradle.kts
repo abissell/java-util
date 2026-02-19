f4928c77b67109524e0abe6122030b9dee0753b9
Andrew Bissell <abissell@gmail.com>
2026-02-19 02:20:51 +0000

    add a Gradle build setup

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api(libs.net.openhft.chronicle.core)
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
